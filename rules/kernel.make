clemensg/ptxdist

commit cd920ab716bab0c190666e0273c10b19c7cdaeec

barebox/kernel: drop explicit dependency
This is now autogenerated by ptxd_lib_dgen.

Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/kernel.make
@@ -105,7 +105,7 @@ endif
 #
 KERNEL_INITRAMFS_SOURCE_$(PTXCONF_IMAGE_KERNEL_INITRAMFS) += $(STATEDIR)/empty.cpio
 
-$(STATEDIR)/kernel.prepare: $(KERNEL_CONFIG)
+$(STATEDIR)/kernel.prepare:
 	@$(call targetinfo)
 
 	@$(call world/kconfig-setup, KERNEL)

rules/barebox.make
@@ -75,7 +75,7 @@ BAREBOX_EXTRA_ENV_DEPS := \
 $(STATEDIR)/barebox.prepare: $(BAREBOX_EXTRA_ENV_DEPS)
 endif
 
-$(STATEDIR)/barebox.prepare: $(BAREBOX_CONFIG)
+$(STATEDIR)/barebox.prepare:
 	@$(call targetinfo)
 
 	@$(call world/kconfig-setup, BAREBOX)

rules/templates/template-barebox-make
@@ -60,7 +60,7 @@ $(BAREBOX_@PACKAGE@_CONFIG):
 	@exit 1
 endif
 
-$(STATEDIR)/barebox-@package@.prepare: $(BAREBOX_@PACKAGE@_CONFIG)
+$(STATEDIR)/barebox-@package@.prepare:
 
 # ----------------------------------------------------------------------------
 # Install

rules/templates/template-kernel-make
@@ -57,7 +57,7 @@ $(KERNEL_@PACKAGE@_CONFIG):
 	@exit 1
 endif
 
-$(STATEDIR)/kernel-@package@.prepare: $(KERNEL_@PACKAGE@_CONFIG)
+$(STATEDIR)/kernel-@package@.prepare:
 
 # ----------------------------------------------------------------------------
 # Compile

commit eb60e68ab20fb8d6c14ab113f9fa5f5dc8294478

kernel: rework prepare stage
The old implementation is broken, when a initramfs is used.
Changing the kconfig symbol back to '# Automatically set by PTXDist' is
broken because '$(<)' is not the kernel config any more (Broken since
cd920ab716bab0c190666e0273c10b19c7cdaeec "barebox/kernel: drop explicit
dependency").
Also, world/kconfig-sync compares .config and KERNEL_CONFIG and this does
not work because CONFIG_INITRAMFS_SOURCE in .config has been modified.

Instead, make sure that KERNEL_CONFIG contains the path independent dummy
text before copying it to .config.
Then run the default prepare stage that checks or updates the config.
Insert the path for the empty initramfs after all checks are done.

Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>
Tested-by: Jon Ringle <jringle@gridpoint.com>

rules/kernel.make
@@ -105,30 +105,31 @@ KERNEL_INITRAMFS_SOURCE_$(PTXCONF_IMAGE_KERNEL_INITRAMFS) += $(STATEDIR)/empty.c
 
 $(STATEDIR)/kernel.prepare:
 	@$(call targetinfo)
-
-	@$(call world/kconfig-setup, KERNEL)
+#
+# Make sure there is a non empty INITRAMFS_SOURCE in $(KERNEL_CONFIG), but
+# not the real expanded path because it contains local workdir path which
+# is not relevant to other developers.
+#
+ifdef KERNEL_INITRAMFS_SOURCE_y
+	@sed -i -e 's,^CONFIG_INITRAMFS_SOURCE.*$$,CONFIG_INITRAMFS_SOURCE=\"# Automatically set by PTXDist\",g' \
+		"$(KERNEL_CONFIG)"
+endif
 ifdef PTXCONF_KERNEL_IMAGE_SIMPLE
 	cp $(PTXCONF_KERNEL_IMAGE_SIMPLE_DTS) \
 		$(KERNEL_DIR)/arch/$(PTXCONF_KERNEL_ARCH_STRING)/boot/dts/$(PTXCONF_KERNEL_IMAGE_SIMPLE_TARGET).dts
 endif
 
+	@$(call world/prepare, KERNEL)
+
+#
+# Use a existing dummy INITRAMFS_SOURCE for the fist 'make' call. The
+# kernel image will be rebuilt in the image-kernel package with the real
+# initramfs.
+#
 ifdef KERNEL_INITRAMFS_SOURCE_y
 	@touch "$(KERNEL_INITRAMFS_SOURCE_y)"
 	@sed -i -e 's,^CONFIG_INITRAMFS_SOURCE.*$$,CONFIG_INITRAMFS_SOURCE=\"$(KERNEL_INITRAMFS_SOURCE_y)\",g' \
 		"$(KERNEL_DIR)/.config"
-endif
-
-	@$(call ptx/oldconfig, KERNEL)
-	@$(call world/kconfig-sync, KERNEL)
-
-#
-# Don't keep the expanded path to INITRAMS_SOURCE in $(KERNEL_CONFIG),
-# because it contains local workdir path which is not relevant to
-# other developers.
-#
-ifdef KERNEL_INITRAMFS_SOURCE_y
-	@sed -i -e 's,^CONFIG_INITRAMFS_SOURCE.*$$,CONFIG_INITRAMFS_SOURCE=\"# Automatically set by PTXDist\",g' \
-		"$(<)"
 endif
 	@$(call touch)
 