clemensg/ptxdist

commit d9ee5ecd1fe89b4a1cc3a32b09388ab678d129af

lighttpd: add license information
Signed-off-by: Andreas Pretzsch <apr@cn-eng.de>
Reviewed-by: Philipp Zabel <p.zabel@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/lighttpd.make
@@ -24,6 +24,7 @@ LIGHTTPD_SUFFIX		:= tar.xz
 LIGHTTPD_URL		:= http://download.lighttpd.net/lighttpd/releases-1.4.x/$(LIGHTTPD).$(LIGHTTPD_SUFFIX)
 LIGHTTPD_SOURCE		:= $(SRCDIR)/$(LIGHTTPD).$(LIGHTTPD_SUFFIX)
 LIGHTTPD_DIR		:= $(BUILDDIR)/$(LIGHTTPD)
+LIGHTTPD_LICENSE	:= BSD
 
 # ----------------------------------------------------------------------------
 # Prepare

commit 63096a65c56a38a3e2b5631ed21b4e66272c897b

lighttpd: Fix licenses and add more license file hashes
Most of lighttpd is BSD-3-Clause, except three files, which are OML or
RSA-MD licensed. Found by reading Debian package copyright information
and by inspecting file headers.

Fixes: d9ee5ecd1fe89b4a1cc3a32b09388ab678d129af
Cc: Andreas Pretzsch <apr@cn-eng.de>
Signed-off-by: Alexander Dahl <ada@thorsis.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/lighttpd.make
@@ -24,8 +24,11 @@ LIGHTTPD_SUFFIX		:= tar.xz
 LIGHTTPD_URL		:= http://download.lighttpd.net/lighttpd/releases-1.4.x/$(LIGHTTPD).$(LIGHTTPD_SUFFIX)
 LIGHTTPD_SOURCE		:= $(SRCDIR)/$(LIGHTTPD).$(LIGHTTPD_SUFFIX)
 LIGHTTPD_DIR		:= $(BUILDDIR)/$(LIGHTTPD)
-LIGHTTPD_LICENSE	:= BSD-3-Clause
-LIGHTTPD_LICENSE_FILES := file://COPYING;md5=e4dac5c6ab169aa212feb5028853a579
+LIGHTTPD_LICENSE	:= BSD-3-Clause AND OML AND RSA-MD
+LIGHTTPD_LICENSE_FILES	:= \
+	file://COPYING;md5=e4dac5c6ab169aa212feb5028853a579 \
+	file://src/fastcgi.h;startline=7;endline=15;md5=fe9ffe753772839aace9c90e814bc356 \
+	file://src/md5.c;startline=6;endline=26;md5=b5be3b6afd4afa7bb89b16361244f9b6
 
 # ----------------------------------------------------------------------------
 # Prepare