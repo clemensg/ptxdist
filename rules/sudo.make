clemensg/ptxdist

commit 8a4ca3129944e590408f375aa4855a7e7509d137

sudo: add license information
Signed-off-by: Andreas Pretzsch <apr@cn-eng.de>
Reviewed-by: Philipp Zabel <p.zabel@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/sudo.make
@@ -26,6 +26,7 @@ SUDO_URL	:= \
 	http://www.sudo.ws/sudo/dist/OLD/$(SUDO).$(SUDO_SUFFIX)
 SUDO_SOURCE	:= $(SRCDIR)/$(SUDO).$(SUDO_SUFFIX)
 SUDO_DIR	:= $(BUILDDIR)/$(SUDO)
+SUDO_LICENSE	:= BSD, ISC
 
 # ----------------------------------------------------------------------------
 # Prepare

commit e9b6ad9a7e324241b2e45dd1c99a13e212d6d3bc

sudo: Fix licenses and add license file hash
Upstream distributes a fairly complete LICENSE document listing all
applying licenses.

Fixes: 8a4ca3129944e590408f375aa4855a7e7509d137
Cc: Andreas Pretzsch <apr@cn-eng.de>
Signed-off-by: Alexander Dahl <ada@thorsis.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/sudo.make
@@ -26,7 +26,8 @@ SUDO_URL	:= \
 	http://www.sudo.ws/sudo/dist/OLD/$(SUDO).$(SUDO_SUFFIX)
 SUDO_SOURCE	:= $(SRCDIR)/$(SUDO).$(SUDO_SUFFIX)
 SUDO_DIR	:= $(BUILDDIR)/$(SUDO)
-SUDO_LICENSE	:= BSD AND ISC
+SUDO_LICENSE	:= ISC AND BSD-3-Clause AND BSD-2-Clause-NetBSD AND Zlib
+SUDO_LICENSE_FILES := file://doc/LICENSE;md5=7765a3d787cb4fed3ccc3c9cee030af9
 
 # ----------------------------------------------------------------------------
 # Prepare