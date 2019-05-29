clemensg/ptxdist

commit db4d91b62aa8f8653fa2e6b87fc72f07cd838cfd

libnftnl: add new library package in version 1.0.5
	- lbnftnl will be used by
	  - iptable version 1.6.0
	  - nftables version 0.5

Signed-off-by: Andreas Geisenhainer <Andreas.Geisenhainer@atsonline.de>
[mol: add missing dependency]
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/libnftnl.make
@@ -1,0 +1,56 @@
+# -*-makefile-*-
+#
+# Copyright (C) 2016 by Andreas Geisenhainer <andreas.geisenhainer@atsonline.de>
+#
+# See CREDITS for details about who has contributed to this project.
+#
+# For further information about the PTXdist project and license conditions
+# see the README file.
+#
+
+#
+# We provide this package
+#
+PACKAGES-$(PTXCONF_LIBNFTNL) += libnftnl
+
+#
+# Paths and names
+#
+LIBNFTNL_VERSION	:= 1.0.5
+LIBNFTNL_MD5	:= af0c62ce6bbd7a7d39def0996c1c17c9
+LIBNFTNL		:= libnftnl-$(LIBNFTNL_VERSION)
+LIBNFTNL_SUFFIX	:= tar.bz2
+LIBNFTNL_URL		:= http://ftp.netfilter.org/pub/libnftnl/$(LIBNFTNL).$(LIBNFTNL_SUFFIX)
+LIBNFTNL_SOURCE	:= $(SRCDIR)/$(LIBNFTNL).$(LIBNFTNL_SUFFIX)
+LIBNFTNL_DIR		:= $(BUILDDIR)/$(LIBNFTNL)
+LIBNFTNL_LICENSE	:= GPL-2.0
+
+#
+# autoconf
+#
+LIBNFTNL_CONF_TOOL	:= autoconf
+LIBNFTNL_CONF_OPT	:= \
+	$(CROSS_AUTOCONF_USR) \
+	--$(call ptx/wwo, PTXCONF_LIBNFTNL_XML)-xml-parsing \
+	--$(call ptx/wwo, PTXCONF_LIBNFTNL_JSON)-json-parsing
+
+# ----------------------------------------------------------------------------
+# Target-Install
+# ----------------------------------------------------------------------------
+
+$(STATEDIR)/libnftnl.targetinstall:
+	@$(call targetinfo)
+
+	@$(call install_init, libnftnl)
+	@$(call install_fixup, libnftnl,PRIORITY,optional)
+	@$(call install_fixup, libnftnl,SECTION,base)
+	@$(call install_fixup, libnftnl,AUTHOR,"Andreas Geisenhainer <andreas.geisenhainer@atsonline.de>")
+	@$(call install_fixup, libnftnl,DESCRIPTION,missing)
+
+	@$(call install_lib, libnftnl, 0, 0, 0644, libnftnl)
+
+	@$(call install_finish, libnftnl)
+
+	@$(call touch)
+
+# vim: syntax=make

rules/libnftnl.in
@@ -1,0 +1,27 @@
+## SECTION=networking
+
+menuconfig LIBNFTNL
+	tristate
+	select LIBMNL
+	select LIBMXML if LIBNFTNL_XML
+	select JANSSON if LIBNFTNL_JSON
+	prompt "libnftnl                      "
+	help
+	  libnftnl is a userspace library providing a low-level netlink
+	  programming interface (API) to the in-kernel nf_tables subsystem.
+	  The library libnftnl has been previously known as libnftables.
+	  This library is currently used by nftables.
+
+if LIBNFTNL
+
+config LIBNFTNL_XML
+	bool
+	depends on LIBNFTNL
+	prompt "XML parsing support"
+
+config LIBNFTNL_JSON
+	bool
+	depends on LIBNFTNL
+	prompt "JSON parsing support"
+
+endif

commit 005fcb69edb0f5fb92cc3b28f3013b658974c2ad

libnftnl: Fix license and add license file hash
When added to ptxdist in 2016 the package version was 1.0.5, which
already contained at least one file under GPL-2.0-only, which is still
the case with the current version 1.1.2.

Fixes: db4d91b62aa8f8653fa2e6b87fc72f07cd838cfd
Cc: Andreas Geisenhainer <Andreas.Geisenhainer@atsonline.de>
Signed-off-by: Alexander Dahl <ada@thorsis.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/libnftnl.make
@@ -23,7 +23,8 @@ LIBNFTNL_SUFFIX		:= tar.bz2
 LIBNFTNL_URL		:= http://ftp.netfilter.org/pub/libnftnl/$(LIBNFTNL).$(LIBNFTNL_SUFFIX)
 LIBNFTNL_SOURCE		:= $(SRCDIR)/$(LIBNFTNL).$(LIBNFTNL_SUFFIX)
 LIBNFTNL_DIR		:= $(BUILDDIR)/$(LIBNFTNL)
-LIBNFTNL_LICENSE	:= GPL-2.0-only
+LIBNFTNL_LICENSE	:= GPL-2.0-only AND GPL-2.0-or-later
+LIBNFTNL_LICENSE_FILES	:= file://COPYING;md5=79808397c3355f163c012616125c9e26
 
 #
 # autoconf