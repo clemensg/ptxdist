clemensg/ptxdist

commit 9d22b77f54f0001640e4d632d0c463c029a3e8d0

libgmp: version bump 4.2.4 -> 6.0.0a
Signed-off-by: Alexander Aring <alex.aring@gmail.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/libgmp.make
@@ -17,8 +17,8 @@ PACKAGES-$(PTXCONF_LIBGMP) += libgmp
 #
 # Paths and names
 #
-LIBGMP_VERSION	:= 4.2.4
-LIBGMP_MD5	:= fc1e3b3a2a5038d4d74138d0b9cf8dbe
+LIBGMP_VERSION	:= 6.0.0a
+LIBGMP_MD5	:= b7ff2d88cae7f8085bd5006096eed470
 LIBGMP		:= gmp-$(LIBGMP_VERSION)
 LIBGMP_SUFFIX	:= tar.bz2
 LIBGMP_URL	:= $(call ptx/mirror, GNU, gmp/$(LIBGMP).$(LIBGMP_SUFFIX))

commit a489d377374c88eb3f089390ea4b6636617b2ac8

libgmp: Fix license
The README of libgmp 6.1.2 and 6.0.0 states this:

> The GNU MP Library is free software; you can redistribute it and/or modify
> it under the terms of either:
>
>   * the GNU Lesser General Public License as published by the Free
>     Software Foundation; either version 3 of the License, or (at your
>     option) any later version.
>
> or
>
>   * the GNU General Public License as published by the Free Software
>     Foundation; either version 2 of the License, or (at your option) any
>     later version.
>
> or both in parallel, as here.

The library switched licensing with release of version 6.

Fixes: 9d22b77f54f0001640e4d632d0c463c029a3e8d0
Cc: Alexander Aring <alex.aring@gmail.com>
Fixes: 00c6e91b6b8a54cdfc7af0d75e4eb7637981e63d
Cc: Juergen Borleis <jbe@pengutronix.de>
Signed-off-by: Alexander Dahl <ada@thorsis.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/libgmp.make
@@ -24,7 +24,7 @@ LIBGMP_SUFFIX	:= tar.bz2
 LIBGMP_URL	:= $(call ptx/mirror, GNU, gmp/$(LIBGMP).$(LIBGMP_SUFFIX))
 LIBGMP_SOURCE	:= $(SRCDIR)/$(LIBGMP).$(LIBGMP_SUFFIX)
 LIBGMP_DIR	:= $(BUILDDIR)/$(LIBGMP)
-LIBGMP_LICENSE	:= GPL-3.0-only AND LGPL-3.0-only
+LIBGMP_LICENSE	:= GPL-2.0-or-later OR LGPL-3.0-or-later
 
 # ----------------------------------------------------------------------------
 # Prepare