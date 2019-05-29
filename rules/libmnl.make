clemensg/ptxdist

commit e2c4c9dceec7dfcef3bfcbd2f2a2d09eb88b8726

libmnl: adding new library in version 1.0.3
	- library will be used by nftables

Signed-off-by: Andreas Geisenhainer <Andreas.Geisenhainer@atsonline.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/libmnl.make
@@ -1,0 +1,52 @@
+# -*-makefile-*-
+#
+# Copyright (C) 2016 by Andreas Geisenhainer <andreas.geisenhainer@atsonline.de>
+#
+# See CREDITS for details about who has contributed to this project.
+#
+# For further information about the PTXdist project and license conditions
+# see the README file.
+#
+
+#
+# We provide this package
+#
+PACKAGES-$(PTXCONF_LIBMNL) += libmnl
+
+#
+# Paths and names
+#
+LIBMNL_VERSION	:= 1.0.3
+LIBMNL_MD5	:= 7d95fc3bea3365bc03c48e484224f65f
+LIBMNL		:= libmnl-$(LIBMNL_VERSION)
+LIBMNL_SUFFIX	:= tar.bz2
+LIBMNL_URL		:= http://ftp.netfilter.org/pub/libmnl/$(LIBMNL).$(LIBMNL_SUFFIX)
+LIBMNL_SOURCE	:= $(SRCDIR)/$(LIBMNL).$(LIBMNL_SUFFIX)
+LIBMNL_DIR		:= $(BUILDDIR)/$(LIBMNL)
+LIBMNL_LICENSE	:= GPL-2.0
+
+#
+# autoconf
+#
+LIBMNL_CONF_TOOL	:= autoconf
+
+# ----------------------------------------------------------------------------
+# Target-Install
+# ----------------------------------------------------------------------------
+
+$(STATEDIR)/libmnl.targetinstall:
+	@$(call targetinfo)
+
+	@$(call install_init, libmnl)
+	@$(call install_fixup, libmnl,PRIORITY,optional)
+	@$(call install_fixup, libmnl,SECTION,base)
+	@$(call install_fixup, libmnl,AUTHOR,"Andreas Geisenhainer <andreas.geisenhainer@atsonline.de>")
+	@$(call install_fixup, libmnl,DESCRIPTION,missing)
+
+	@$(call install_lib, libmnl, 0, 0, 0644, libmnl)
+
+	@$(call install_finish, libmnl)
+
+	@$(call touch)
+
+# vim: syntax=make

rules/libmnl.in
@@ -1,0 +1,11 @@
+## SECTION=networking
+
+config LIBMNL
+	bool
+	prompt "libmnl"
+	help
+		libmnl is a minimalistic user-space library oriented to Netlink
+		developers. There are a lot of common tasks in parsing, validating,
+		constructing of both the Netlink header and TLVs that are repetitive
+		and easy to get wrong. This library aims to provide simple helpers
+		that allows you to re-use code and to avoid re-inventing the wheel.

commit 3964e5b387868fefe5d3a1b88035de9fc7b8cb4a

libmnl: Fix license and add license file hashes
libmnl switched from GPLv2+ to LGPLv2.1+ before the 1.0.0 release. The
package was initially added to ptxdist with version 1.0.3 in 2016 with
the wrong license information. The project clearly states it is
LGPL-2.1-or-later in both COPYING file and file headers.

Fixes: e2c4c9dceec7dfcef3bfcbd2f2a2d09eb88b8726
Cc: Andreas Geisenhainer <Andreas.Geisenhainer@atsonline.de>
Signed-off-by: Alexander Dahl <ada@thorsis.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/libmnl.make
@@ -23,7 +23,8 @@ LIBMNL_SUFFIX	:= tar.bz2
 LIBMNL_URL		:= http://ftp.netfilter.org/pub/libmnl/$(LIBMNL).$(LIBMNL_SUFFIX)
 LIBMNL_SOURCE	:= $(SRCDIR)/$(LIBMNL).$(LIBMNL_SUFFIX)
 LIBMNL_DIR		:= $(BUILDDIR)/$(LIBMNL)
-LIBMNL_LICENSE	:= GPL-2.0-only
+LIBMNL_LICENSE	:= LGPL-2.1-or-later
+LIBMNL_LICENSE_FILES := file://COPYING;md5=4fbd65380cdd255951079008b364516c
 
 #
 # autoconf