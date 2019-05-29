clemensg/ptxdist

commit 4cb0b96e0df94644e42a735039967c6a5fa22a27

net-snmp: version bump to 5.7.3 LTS
Version bump to net-snmp version 5.7.3

Signed-off-by: Oliver Graute <oliver.graute@neuhaus.de>
[mol: fix series file]
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/net-snmp.make
@@ -18,13 +18,14 @@ PACKAGES-$(PTXCONF_NET_SNMP) += net-snmp
 #
 # Paths and names
 #
-NET_SNMP_VERSION	:= 5.7.2
-NET_SNMP_MD5		:= 5bddd02e2f82b62daa79f82717737a14
+NET_SNMP_VERSION	:= 5.7.3
+NET_SNMP_MD5		:= d4a3459e1577d0efa8d96ca70a885e53
 NET_SNMP		:= net-snmp-$(NET_SNMP_VERSION)
 NET_SNMP_SUFFIX		:= tar.gz
 NET_SNMP_URL		:= $(call ptx/mirror, SF, net-snmp/$(NET_SNMP).$(NET_SNMP_SUFFIX))
 NET_SNMP_SOURCE		:= $(SRCDIR)/$(NET_SNMP).$(NET_SNMP_SUFFIX)
 NET_SNMP_DIR		:= $(BUILDDIR)/$(NET_SNMP)
+NET_LICENSE		:= BSD like
 
 # ----------------------------------------------------------------------------
 # Prepare

patches/net-snmp-5.7.2/0001-bug-fix-from-fenner-fix-ICMP-mib-table-handling-on-l.patch
@@ -1,148 +1,0 @@
-From: Wes Hardaker <hardaker@users.sourceforge.net>
-Date: Wed, 19 Feb 2014 15:21:57 -0800
-Subject: [PATCH] bug fix from fenner: fix ICMP mib table handling on linux
-
----
- agent/mibgroup/mibII/icmp.c         |   25 +++++++++++++++++++++++--
- agent/mibgroup/mibII/kernel_linux.c |   19 +++++++++++++------
- agent/mibgroup/mibII/kernel_linux.h |    4 ++--
- 3 files changed, 38 insertions(+), 10 deletions(-)
-
-diff --git a/agent/mibgroup/mibII/icmp.c b/agent/mibgroup/mibII/icmp.c
-index 14c73a6..6d10426 100644
---- a/agent/mibgroup/mibII/icmp.c
-+++ b/agent/mibgroup/mibII/icmp.c
-@@ -106,10 +106,20 @@ struct icmp_msg_stats_table_entry {
-         int flags;
- };
- 
-+#ifdef linux
-+/* Linux keeps track of all possible message types */
-+#define ICMP_MSG_STATS_IPV4_COUNT 256
-+#else
- #define ICMP_MSG_STATS_IPV4_COUNT 11
-+#endif
- 
- #ifdef NETSNMP_ENABLE_IPV6
-+#ifdef linux
-+/* Linux keeps track of all possible message types */
-+#define ICMP_MSG_STATS_IPV6_COUNT 256
-+#else
- #define ICMP_MSG_STATS_IPV6_COUNT 14
-+#endif
- #else
- #define ICMP_MSG_STATS_IPV6_COUNT 0
- #endif /* NETSNMP_ENABLE_IPV6 */
-@@ -177,7 +187,7 @@ icmp_msg_stats_load(netsnmp_cache *cache, void *vmagic)
-     inc = 0;
-     linux_read_icmp_msg_stat(&v4icmp, &v4icmpmsg, &flag);
-     if (flag) {
--        while (254 != k) {
-+        while (255 >= k) {
-             if (v4icmpmsg.vals[k].InType) {
-                 icmp_msg_stats_table[i].ipVer = 1;
-                 icmp_msg_stats_table[i].icmpMsgStatsType = k;
-@@ -267,7 +277,7 @@ icmp_msg_stats_load(netsnmp_cache *cache, void *vmagic)
-     inc = 0;
-     linux_read_icmp6_msg_stat(&v6icmp, &v6icmpmsg, &flag);
-     if (flag) {
--        while (254 != k) {
-+        while (255 >= k) {
-             if (v6icmpmsg.vals[k].InType) {
-                 icmp_msg_stats_table[i].ipVer = 2;
-                 icmp_msg_stats_table[i].icmpMsgStatsType = k;
-@@ -1050,6 +1060,12 @@ icmp_stats_table_handler(netsnmp_mib_handler  *handler,
- 					continue;
- 				table_info = netsnmp_extract_table_info(request);
- 				subid      = table_info->colnum;
-+				DEBUGMSGTL(( "mibII/icmpStatsTable", "oid: " ));
-+				DEBUGMSGOID(( "mibII/icmpStatsTable", request->requestvb->name,
-+						 request->requestvb->name_length ));
-+				DEBUGMSG(( "mibII/icmpStatsTable", " In %d InErr %d Out %d OutErr %d\n",
-+					      entry->icmpStatsInMsgs, entry->icmpStatsInErrors,
-+					      entry->icmpStatsOutMsgs, entry->icmpStatsOutErrors ));
- 
- 				switch (subid) {
- 					case ICMP_STAT_INMSG:
-@@ -1117,6 +1133,11 @@ icmp_msg_stats_table_handler(netsnmp_mib_handler          *handler,
-                     continue;
-                 table_info = netsnmp_extract_table_info(request);
-                 subid = table_info->colnum;
-+                DEBUGMSGTL(( "mibII/icmpMsgStatsTable", "oid: " ));
-+                DEBUGMSGOID(( "mibII/icmpMsgStatsTable", request->requestvb->name,
-+                                 request->requestvb->name_length ));
-+                DEBUGMSG(( "mibII/icmpMsgStatsTable", " In %d Out %d Flags 0x%x\n",
-+                                 entry->icmpMsgStatsInPkts, entry->icmpMsgStatsOutPkts, entry->flags ));
- 
-                 switch (subid) {
-                     case ICMP_MSG_STAT_IN_PKTS:
-diff --git a/agent/mibgroup/mibII/kernel_linux.c b/agent/mibgroup/mibII/kernel_linux.c
-index b21a166..ba320c7 100644
---- a/agent/mibgroup/mibII/kernel_linux.c
-+++ b/agent/mibgroup/mibII/kernel_linux.c
-@@ -81,9 +81,9 @@ decode_icmp_msg(char *line, char *data, struct icmp4_msg_mib *msg)
-             index = strtol(token, &delim, 0);
-             if (ERANGE == errno) {
-                 continue;
--            } else if (index > LONG_MAX) {
-+            } else if (index > 255) {
-                 continue;
--            } else if (index < LONG_MIN) {
-+            } else if (index < 0) {
-                 continue;
-             }
-             if (NULL == (token = strtok_r(dataptr, " ", &saveptr1)))
-@@ -94,9 +94,9 @@ decode_icmp_msg(char *line, char *data, struct icmp4_msg_mib *msg)
-             index = strtol(token, &delim, 0);
-             if (ERANGE == errno) {
-                 continue;
--            } else if (index > LONG_MAX) {
-+            } else if (index > 255) {
-                 continue;
--            } else if (index < LONG_MIN) {
-+            } else if (index < 0) {
-                 continue;
-             }
-             if(NULL == (token = strtok_r(dataptr, " ", &saveptr1)))
-@@ -426,14 +426,21 @@ linux_read_icmp6_parse(struct icmp6_mib *icmp6stat,
- 
-         vals = name;
-         if (NULL != icmp6msgstat) {
-+            int type;
-             if (0 == strncmp(name, "Icmp6OutType", 12)) {
-                 strsep(&vals, "e");
--                icmp6msgstat->vals[atoi(vals)].OutType = stats;
-+                type = atoi(vals);
-+                if ( type < 0 || type > 255 )
-+                    continue;
-+                icmp6msgstat->vals[type].OutType = stats;
-                 *support = 1;
-                 continue;
-             } else if (0 == strncmp(name, "Icmp6InType", 11)) {
-                 strsep(&vals, "e");
--                icmp6msgstat->vals[atoi(vals)].InType = stats;
-+                type = atoi(vals);
-+                if ( type < 0 || type > 255 )
-+                    continue;
-+                icmp6msgstat->vals[type].InType = stats;
-                 *support = 1;
-                 continue;
-             }
-diff --git a/agent/mibgroup/mibII/kernel_linux.h b/agent/mibgroup/mibII/kernel_linux.h
-index 6bf5d47..c6dfca9 100644
---- a/agent/mibgroup/mibII/kernel_linux.h
-+++ b/agent/mibgroup/mibII/kernel_linux.h
-@@ -121,11 +121,11 @@ struct icmp_msg_mib {
- 
- /* Lets use wrapper structures for future expansion */
- struct icmp4_msg_mib {
--    struct icmp_msg_mib vals[255];
-+    struct icmp_msg_mib vals[256];
- };
- 
- struct icmp6_msg_mib {
--    struct icmp_msg_mib vals[255];
-+    struct icmp_msg_mib vals[256];
- };
- 
- struct udp_mib {

patches/net-snmp-5.7.3/0200-net-snmp-config-add-SYSROOT-support.patch
@@ -12,11 +12,11 @@ Adapted to 5.7.2
 
 Signed-off-by: Alexander Dahl <post@lespocky.de>
 ---
- net-snmp-config.in |    8 ++++----
+ net-snmp-config.in | 8 ++++----
  1 file changed, 4 insertions(+), 4 deletions(-)
 
 diff --git a/net-snmp-config.in b/net-snmp-config.in
-index 2300fe1..361c658 100644
+index d55872cdd50a..c8611bcc7ebe 100644
 --- a/net-snmp-config.in
 +++ b/net-snmp-config.in
 @@ -54,7 +54,7 @@ NSC_LAGENTLIBS="@LAGENTLIBS@ @PERLLDOPTS_FOR_APPS@"

patches/net-snmp-5.7.3/0201-Don-t-disable-udp-and-tcp-when-disable-agentx-dom-so.patch
@@ -1,7 +1,7 @@
 From: =?UTF-8?q?Uwe=20Kleine-K=C3=B6nig?= <u.kleine-koenig@pengutronix.de>
 Date: Tue, 3 Sep 2013 21:12:46 +0200
-Subject: [PATCH] Don't disable udp and tcp when
- --disable-agentx-dom-sock-only is given
+Subject: [PATCH] Don't disable udp and tcp when --disable-agentx-dom-sock-only
+ is given
 
 The configure magic without this patch treats all
 
@@ -13,11 +13,11 @@ the same; i.e. disables the udp and tcp transports. With this patch only the
 first option does have this effect which matches the usual and expected
 semantic.
 ---
- configure.d/config_project_with_enable |    8 +++++---
+ configure.d/config_project_with_enable | 8 +++++---
  1 file changed, 5 insertions(+), 3 deletions(-)
 
 diff --git a/configure.d/config_project_with_enable b/configure.d/config_project_with_enable
-index 61ba026..d7c2132 100644
+index 61ba026f355d..d7c2132a0bd4 100644
 --- a/configure.d/config_project_with_enable
 +++ b/configure.d/config_project_with_enable
 @@ -723,9 +723,11 @@ NETSNMP_ARG_ENABLE(local-smux,

patches/net-snmp-5.7.3/series
@@ -1,8 +1,7 @@
 # generated by git-ptx-patches
 #tag:base --start-number 1
 #tag:upstream --start-number 1
-0001-bug-fix-from-fenner-fix-ICMP-mib-table-handling-on-l.patch
 #tag:ptx --start-number 200
 0200-net-snmp-config-add-SYSROOT-support.patch
 0201-Don-t-disable-udp-and-tcp-when-disable-agentx-dom-so.patch
-# eda93cf4517290230962af7514bfd246  - git-ptx-patches magic
+# 1f82ffbca883f30fa71dcd5f51d85e41  - git-ptx-patches magic

commit 25174d3a61d28d82d3562223efb70556d943b8e6

net-snmp: Fix licenses and add license file hash
Fixes: 4cb0b96e0df94644e42a735039967c6a5fa22a27
Cc: Oliver Graute <oliver.graute@gmail.com>
Signed-off-by: Alexander Dahl <ada@thorsis.com>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/net-snmp.make
@@ -25,7 +25,8 @@ NET_SNMP_SUFFIX		:= tar.gz
 NET_SNMP_URL		:= $(call ptx/mirror, SF, net-snmp/$(NET_SNMP).$(NET_SNMP_SUFFIX))
 NET_SNMP_SOURCE		:= $(SRCDIR)/$(NET_SNMP).$(NET_SNMP_SUFFIX)
 NET_SNMP_DIR		:= $(BUILDDIR)/$(NET_SNMP)
-NET_LICENSE		:= BSD like
+NET_SNMP_LICENSE	:= MIT-CMU AND BSD-3-Clause
+NET_SNMP_LICENSE_FILES	:= file://COPYING;md5=5dedd290b5ffbe69d9625ddeb350a044
 
 # ----------------------------------------------------------------------------
 # Prepare