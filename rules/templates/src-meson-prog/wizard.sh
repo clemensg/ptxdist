clemensg/ptxdist

commit 91749a01cd22dc2a7d72e841568a2dddb16200f7

templates: add src-meson-prog template
Signed-off-by: Ulrich Ölmann <u.oelmann@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/templates/src-meson-prog/COPYING
@@ -1,0 +1,12 @@
+Copyright (C) 2006 by Rob Landley <rob@landley.net>
+
+Permission to use, copy, modify, and/or distribute this software for any purpose
+with or without fee is hereby granted.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+THIS SOFTWARE.

rules/templates/src-meson-prog/wizard.sh
@@ -1,0 +1,17 @@
+#!/bin/bash
+
+NAME="${1}"
+if [ -z "$NAME" ]; then
+	echo -n "project name: "
+	read NAME
+fi
+
+mv "@name@.c" "${NAME}.c"
+
+for i in \
+	meson.build \
+	${NAME}.c \
+; do
+	sed -i -e "s/\@name\@/${NAME}/g" $i
+done
+

rules/templates/src-meson-prog/@name@.c
@@ -1,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "config.h"
+
+int main(int argc, char *argv[])
+{
+	printf("Hello World, I'm @name@!\n");
+	printf("My license is " LICENSE_STR ".\n");
+
+	exit(EXIT_SUCCESS);
+}

rules/templates/src-meson-prog/config.h.in
@@ -1,0 +1,1 @@
+#define LICENSE_STR "@license@"

rules/templates/src-meson-prog/meson.build
@@ -1,0 +1,13 @@
+project('@name@', 'c',
+  license : '0BSD',
+  default_options : ['warning_level=3'])
+
+conf_data = configuration_data()
+conf_data.set('license', meson.project_license()[0])
+configure_file(input : 'config.h.in',
+               output : 'config.h',
+               configuration : conf_data)
+
+executable('@name@',
+           '@name@.c',
+           install : true)

rules/templates/template-src-meson-prog-in
@@ -1,0 +1,8 @@
+## SECTION=@section@
+
+config @PACKAGE@
+	bool
+	prompt "@package@"
+	select HOST_MESON
+	help
+	  FIXME

rules/templates/template-src-meson-prog-make
@@ -1,0 +1,83 @@
+# -*-makefile-*-
+#
+# Copyright (C) @YEAR@ by @AUTHOR@
+#
+# See CREDITS for details about who has contributed to this project.
+#
+# For further information about the PTXdist project and license conditions
+# see the README file.
+#
+
+#
+# We provide this package
+#
+PACKAGES-$(PTXCONF_@PACKAGE@) += @package@
+
+#
+# Paths and names
+#
+@PACKAGE@_VERSION	:= @VERSION@
+@PACKAGE@		:= @package@-$(@PACKAGE@_VERSION)
+@PACKAGE@_URL		:= file://local_src/@package@
+@PACKAGE@_DIR		:= $(BUILDDIR)/$(@PACKAGE@)
+@PACKAGE@_LICENSE	:= 0BSD
+@PACKAGE@_LICENSE_FILES	:= file://COPYING;md5=240269ccb62c2c66a81d8fdd094dd69f
+
+# ----------------------------------------------------------------------------
+# Get
+# ----------------------------------------------------------------------------
+
+#$(@PACKAGE@_SOURCE):
+#	@$(call targetinfo)
+#	@$(call get, @PACKAGE@)
+
+# ----------------------------------------------------------------------------
+# Prepare
+# ----------------------------------------------------------------------------
+
+#@PACKAGE@_CONF_ENV	:= $(CROSS_ENV)
+
+#
+# meson
+#
+@PACKAGE@_CONF_TOOL	:= meson
+#@PACKAGE@_CONF_OPT	:= $(CROSS_MESON_USR)
+
+# ----------------------------------------------------------------------------
+# Target-Install
+# ----------------------------------------------------------------------------
+
+$(STATEDIR)/@package@.targetinstall:
+	@$(call targetinfo)
+
+	@$(call install_init, @package@)
+	@$(call install_fixup, @package@, PRIORITY, optional)
+	@$(call install_fixup, @package@, SECTION, base)
+	@$(call install_fixup, @package@, AUTHOR, "@AUTHOR@")
+	@$(call install_fixup, @package@, DESCRIPTION, missing)
+
+#	#
+#	# example code:; copy all binaries
+#	#
+
+	@for i in $(shell cd $(@PACKAGE@_PKGDIR) && find bin sbin usr/bin usr/sbin -type f); do \
+		$(call install_copy, @package@, 0, 0, 0755, -, /$$i); \
+	done
+
+#	#
+#	# FIXME: add all necessary things here
+#	#
+
+	@$(call install_finish, @package@)
+
+	@$(call touch)
+
+# ----------------------------------------------------------------------------
+# Clean
+# ----------------------------------------------------------------------------
+
+#$(STATEDIR)/@package@.clean:
+#	@$(call targetinfo)
+#	@$(call clean_pkg, @PACKAGE@)
+
+# vim: syntax=make

scripts/lib/ptxd_lib_template.sh
@@ -334,6 +334,13 @@ export -f ptxd_template_new_src_qmake_prog
 ptxd_template_help_list[${#ptxd_template_help_list[@]}]="src-qmake-prog"
 ptxd_template_help_list[${#ptxd_template_help_list[@]}]="create qmake binary"
 
+ptxd_template_new_src_meson_prog() {
+    ptxd_template_src_base
+}
+export -f ptxd_template_new_src_meson_prog
+ptxd_template_help_list[${#ptxd_template_help_list[@]}]="src-meson-prog"
+ptxd_template_help_list[${#ptxd_template_help_list[@]}]="create meson binary"
+
 ptxd_template_new_src_linux_driver() {
     ptxd_template_src_base
 }

commit 0a4d89a705c332efc877344dba19d84a8176c36a

template: src-meson-prog: replace year and author in COPYING too
Fixes: 91749a01cd22dc2a7d72 ("templates: add src-meson-prog template")
Signed-off-by: Roland Hieber <rhi@pengutronix.de>
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

rules/templates/src-meson-prog/COPYING
@@ -1,4 +1,4 @@
-Copyright (C) 2006 by Rob Landley <rob@landley.net>
+Copyright (C) @YEAR@ by @AUTHOR@
 
 Permission to use, copy, modify, and/or distribute this software for any purpose
 with or without fee is hereby granted.

rules/templates/src-meson-prog/wizard.sh
@@ -15,10 +15,15 @@ fi
 mv "@name@.c" "${NAME}.c"
 
 for i in \
+	COPYING \
 	meson.build \
 	"${NAME}.c" \
 ; do
-	sed -i -e "s/\@name\@/${NAME}/g" \
-	       -e "s/\@version\@/${VERSION}/g" $i
+	sed -i \
+	       -e "s/\@AUTHOR\@/${AUTHOR}/g" \
+	       -e "s/\@YEAR\@/${YEAR}/g" \
+	       -e "s/\@name\@/${NAME}/g" \
+	       -e "s/\@version\@/${VERSION}/g" \
+	       $i
 done
 